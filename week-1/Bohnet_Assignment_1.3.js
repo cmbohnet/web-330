78e4d748de27ab9ee48d890eee4c9a75133e1846
Christine Bohnet <christine.bohnet@gmail.com>
2020-02-21 12:51:04 -0600

    add documentation

--- week-1/Bohnet_Assignment_1.3.js
+++ week-1/Bohnet_Assignment_1.3.js
@@ -29,26 +29,36 @@ console.log(header.display('Christine', 'Bohnet', 'bohnet-assignment-1.3'))
 */
 
 // start program
+
+//Using functions, create a cell phone object with the following properties / methods
+//Manufacturer
+//Model
+//Color
+//Price
+//getPrice()
+//getModel()
+//getDetails()
 function CellPhone(manufacturer) {
 
 this.manufacturer = manufacturer;
 
-this.model = "Pilot";
+this.model = "XP";
 
 this.color = "White";
 
-this.price = 12000;
-
+this.price = 2000;
+//The getPrice() function will return the cell phones price
 this.getPrice = function() {
 
 return this.price;
 };
-
+//The getModel() function will return the cell phones model
 this.getModel = function() {
 
 return this.model;
 
 };
+//The getDetails() function must call the getPrice() and getModel() functions and output the Manufacturer, Model, Color, and Price
 this.getDetails = function() {
 
 return "Manufacturer: " + this.manufacturer + "\n" + "Model: " + this.getModel() +  "\n" + "Color: " + this.color +  "\n" + "Price: " + this.getPrice();
@@ -56,9 +66,11 @@ return "Manufacturer: " + this.manufacturer + "\n" + "Model: " + this.getModel()
 };
 
 }
+//Instantiate a new cell phone object (supply your own default value)
 
-var cell = new CellPhone("Honda");
+var cell = new CellPhone("Apple");
 console.log("-- DISPLAYING CELL PHONE DETAILS --");
+//call the getDetails() function
 console.log(cell.getDetails())
 
 // end program
--- week-1/Bohnet_Assignment_1.4.js
+++ week-1/Bohnet_Assignment_1.4.js
@@ -31,51 +31,59 @@ console.log(header.display('Christine', 'Bohnet', 'bohnet-assignment-1.4'))
 */
 
 // start program
-
+//Create a function constructor Car class with one parameter (model)
 function Car(model) {
   this.model = model;
 }
-
+//Add a prototype method called “start” to output “ added to the racetrack!”
 Car.prototype.start = function(){
   console.log("Car added to the racetrack!");
 };
 
+//Create a function constructor Truck class with two parameters (model, year)
 function Truck(model, year) {
   this.model = model;
   this.year = year;
 }
-
+//Add a prototype method called “start” to output “ added to the racetrack!”
 Truck.prototype.start = function(){
   console.log("Truck added to the racetrack!");
 };
 
+//Create a function constructor Jeep class with three parameters (model, year, color)
 function Jeep(model, year, color) {
   this.model = model;
   this.year = year;
   this.color = color;
 }
-
+//Add a prototype method called “start” to output “ added to the racetrack!”
 Jeep.prototype.start = function(){
   console.log("Jeep added to the racetrack!");
 };
 
+//Define an empty racetrack array
 var racetrack = new Array();
 
+//Create a driveIt function with one parameter (vehicle).  From within the driveIt function call the “start” function on the vehicle
+//vehicle.start and push the vehicle to the racetrack array
 function driveIt(vehicle){
   vehicle.start();
   racetrack.push(vehicle);
 }
 
+//Create three (3) vehicle classes
 var car = new Car("Honda");
 var truck = new Truck("Ford","2012");
 var jeep = new Jeep("Jeep", "2005", "Thundercloud");
 
-driveIt(car);
+// pass them through the driveIt() function
 driveIt(truck);
+driveIt(car);
 driveIt(jeep);
 
 console.log("\n" +  "-- The following vehicles have been added to the racetrack --");
 
+//Iterate over the racetrack array and output the results
 for (var k=0;k<racetrack.length;k++){
   console.log(racetrack[k].constructor.name + ": " + racetrack[k].model);
 }
