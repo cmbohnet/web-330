e40f4b3e95bbee783b3aaf4033553245a9aef619
chris bohnet <cbohnet@my365.bellevue.edu>
2020-04-05 14:13:37 -0500

    Add files via upload

---
+++ week-6/app.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title:  app.js
+; Author: Chris Bohnet
+; Date:   29 March 2020
+; Description: Demonstrates how to create object literals in JavaScript.
+;===========================================
+*/
+
+var person = {
+  firstName: "John",
+  lastName: "Smith"
+};
+
+console.log(person.firstName + " " + person.lastName);
